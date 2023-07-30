f28b90a5475b646cae4fe59d6dbbef91f7d80ae1
APT91 <apt91@tutanota.com>
2023-07-30 04:58:28 -0500

    Fixed Bug

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,6 +1,8 @@
 import os, sys, socket, json, hashlib, colorama, PIL
 import customtkinter as ctk
 
+ctk.set_appearance_mode('dark')
+
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
