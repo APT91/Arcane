d9620cc369e267c041fab7438a23f53bcd74a433
APT91 <apt91@tutanota.com>
2023-07-30 05:06:48 -0500

    Got Raided By Feds

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -3,6 +3,23 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode('dark')
 
+#This window will show the builder options
+class BuilderWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+#This window will be used for settings
+class SettingsWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+#This window will be used for showing developer information
+class AboutWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+
+#Main Window
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
